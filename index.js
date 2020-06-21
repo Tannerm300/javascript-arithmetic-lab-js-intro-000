1de7f13a7bc390003411a1ce3acbd4454ea59186
Tanner McPhee <tannerm296@gmail.com>
2020-06-21 04:22:58 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a,b) {
+  return a + b
+}
+function subtract(a,b) {
+  return a - b
+}
+function multiply(a,b) {
+  return a * b
+}
+function divide(a,b) {
+  return a / b
+}
+function increment(n) {
+  var n = n + 1
+  return n++
+}
+function decrement(n) {
+  var n = n - 1
+  return n--
+}
+function makeInt(n) {
+  parseInt('n', 10)
+  return n
+}
+function preserveDecimal(n) {
+  parseFloat('n')
+  return n
+}
